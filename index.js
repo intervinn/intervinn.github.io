4a756f7e6a0f63b8702cf71b69d6d6e948bf75d4
intervinn <Nikita.Bondarenko335@gmail.com>
2022-04-18 13:19:38 +0300

    new

---
+++ index.js
@@ -0,0 +1,6 @@
+
+function clickHandler() {
+    var oElement = document.getElementById('mybtn');   // get a reference to your element
+    console.log("so pro");
+    oElement.textContent = "so pro";
+}
