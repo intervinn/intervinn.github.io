71fd1455798aafdd939e46d031fcecc98996ecb4
MarkDemidovs <markdemidovs@proton.me>
2026-02-24 19:20:27 +0200

    fixed horizontal scroll bar issue pages/*.tsx

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -2,7 +2,7 @@ import Navbar from "../components/nav";
 
 export default function NotFound() {
     return (
-        <div className="bg-bg min-w-screen min-h-screen flex justify-center items-center flex-col text-text">
+        <div className="bg-bg min-h-screen flex justify-center items-center flex-col text-text">
             <Navbar/>
             <div className="bg-bg2 min-h-screen w-full lg:w-[64rem] px-12 py-12">
                 <h1>Wandering is a terrible sin</h1>
